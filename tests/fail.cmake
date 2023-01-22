031b5ef0094d57aa5e1a520f490f4563c0d0226f
Florian Loitsch <florian@loitsch.com>
2023-01-22 21:32:28 +0100

    fix fail.cmake.

--- tests/fail.cmake
+++ tests/fail.cmake
@@ -18,6 +18,6 @@ set(FAILING_TESTS
 
 if ("${CMAKE_SYSTEM_NAME}" STREQUAL "Windows" OR "${CMAKE_SYSTEM_NAME}" STREQUAL "MSYS")
   list(APPEND TOIT_FAILING_TESTS
-    tests/pipe_crash_test.toit
+    /tests/pipe_crash_test.toit
   )
 endif()
