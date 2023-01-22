6c9d54750979ee18fefc11dfa5707d1749588508
Florian Loitsch <florian@loitsch.com>
2023-01-22 21:33:57 +0100

    Use 'TEST_PREFIX' so it works with opentoit.

--- tests/fail.cmake
+++ tests/fail.cmake
@@ -18,6 +18,6 @@ set(FAILING_TESTS
 
 if ("${CMAKE_SYSTEM_NAME}" STREQUAL "Windows" OR "${CMAKE_SYSTEM_NAME}" STREQUAL "MSYS")
   list(APPEND TOIT_FAILING_TESTS
-    /tests/pipe_crash_test.toit
+    ${TEST_PREFIX}/tests/pipe_crash_test.toit
   )
 endif()
