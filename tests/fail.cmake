da9310960ea836afd84fe4f78efeafd99893e966
Florian Loitsch <florian@loitsch.com>
2023-01-22 21:43:04 +0100

    Fix cmake variable name.

--- tests/fail.cmake
+++ tests/fail.cmake
@@ -17,7 +17,7 @@ set(FAILING_TESTS
 )
 
 if ("${CMAKE_SYSTEM_NAME}" STREQUAL "Windows" OR "${CMAKE_SYSTEM_NAME}" STREQUAL "MSYS")
-  list(APPEND TOIT_FAILING_TESTS
+  list(APPEND FAILING_TESTS
     ${TEST_PREFIX}/tests/pipe_crash_test.toit
   )
 endif()
